b53fc96729c37b1742df89a9edd5d93eafbe6fd4
奥田誠 <lantan9484@gmail.com>
2019-05-13 16:13:43 +0900

    2Dゲームアプリを作ろう 課題
    カリキュラムLesson7：2Dゲームアプリを作ろう の提出課題です。
    Assetフォルダ中のCubeControllerスクリプトに、ブロックが地面や他のブロック衝突時に効果音がなるコードを追記しました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	//キューブの移動速度
+	private float speed = -0.2f;
+	//消滅位置
+	private float deadline = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//キューブを移動させる
+		transform.Translate(this.speed,0,0);
+		//画面外に出たら破棄する
+		if(transform.position.x < this.deadline){
+			Destroy (gameObject);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		//Debug.Log("collision2D呼びだし");
+
+		//衝突先がブロックor地面だった時音を鳴らす
+		if (other.gameObject.tag == "CubeTag" || other.gameObject.tag == "GroundTag") {
+			//Debug.Log ("if呼び出し");
+			AudioSource blocksound = gameObject.GetComponent<AudioSource> ();
+			blocksound.Play();
+		}
+	}
+}
